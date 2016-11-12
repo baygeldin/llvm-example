e4f28a5f4c590235357022c7504250060f5bee51
Alexander Baygeldin <a.baygeldin@gmail.com>
2016-11-12 10:56:04 +0300

    add script for setting environment

---
+++ set_env.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+svn co http://llvm.org/svn/llvm-project/llvm/tags/RELEASE_390/final/ llvm-src
+svn co http://llvm.org/svn/llvm-project/cfe/tags/RELEASE_390/final llvm-src/tools/clang
+cp -R llvm-src llvm-dev
+
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="Sparc" -DLLVM_INCLUDE_TESTS=OFF -DLLVM_INCLUDE_EXAMPLES=OFF -DLLVM_ENABLE_ZLIB=OFF ../llvm-dev
